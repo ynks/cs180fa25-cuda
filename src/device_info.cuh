7671d0722e7bf3848a9e78dfe81d4077c9f19d0b
Dante Harper <dantedigdog@gmail.com>
2025-11-13 16:13:33 +0100

    cool stuff

---
+++ src/device_info.cuh
@@ -0,0 +1,12 @@
+/**
+ * @file device_info.cuh
+ * @author Dante Harper
+ * @date 13/11/25
+ *
+ * @brief A function to print device info
+ */
+
+#pragma once
+
+
+void PrintDeviceInfo();
--- src/sample.cu
+++ src/sample.cu
@@ -1,19 +1,75 @@
 #include <cstdio>
 #include <cuda_runtime.h>
 
-__global__ void test() {
-  // do nothing
+#include "device_info.cuh"
+
+__global__ void VecAdd(int *A, int *B, int *C, int N) {
+  int i = blockDim.x * blockIdx.x + threadIdx.x;
+  if (i < N) {
+    C[i] = A[i] + B[i];
+  }
 }
 
-int main() {
-  // Launch kernel
-  test<<<16, 1024>>>();
+void print(int *arr, int N) {
+  for (int i = 0; i < N; ++i) {
+    printf("%d, ", *arr);
+    arr++;
+  }
+  printf("\n");
+}
 
+// this is a sample program that adds 2 really big vectors together into the C
+// vector
+int main() {
+  PrintDeviceInfo();
+  int N = 48;
+  size_t size = N * sizeof(int);
+
+  // Allocates Memory on Host
+  int *h_A = (int *)malloc(size);
+  int *h_B = (int *)malloc(size);
+  int *h_C = (int *)malloc(size);
+
+  // Initialize Data
+  for (int i = 0; i < N; i++) {
+    h_A[i] = i * 1.0f;
+    h_B[i] = i * 2.0f;
+  }
+
+  // Allocate GLOBAL Memory on Device
+  int *d_A;
+  int *d_B;
+  int *d_C;
+  cudaMalloc(&d_A, size);
+  cudaMalloc(&d_B, size);
+  cudaMalloc(&d_C, size);
+
+  // Copy Memory From Host To Device
+  cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+
+  // Launch kernel
+  VecAdd<<<1024 * 1024 * 2, 1024>>>(d_A, d_B, d_C, N);
   cudaError_t err = cudaDeviceSynchronize();
   if (err != cudaSuccess) {
     printf("CUDA error: %s\n", cudaGetErrorString(err));
     return 1;
   }
+
+  // Copy Memory from Device to Host
+  cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+
+  // Print Stuff
+  print(h_A, N);
+  print(h_B, N);
+  print(h_C, N);
+
+  free(h_A);
+  free(h_B);
+  free(h_C);
+  cudaFree(d_A);
+  cudaFree(d_B);
+  cudaFree(d_C);
 
   printf("Kernel launched successfully!");
   return 0;
